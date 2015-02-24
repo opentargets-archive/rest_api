f07fc7b29700012078ece4d6174d1ae6059eccfd
Andrea Pierleoni <andreap@ebi.ac.uk>
2015-02-24 14:30:07 +0000

    updated conf

--- manage.py
+++ manage.py
@@ -21,7 +21,7 @@ from app import create_app
 from flask.ext.script import Manager, Shell
 # from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('CTTV_API_CONFIG') or 'default')
 manager = Manager(app)
 # migrate = Migrate(app, db)
 
