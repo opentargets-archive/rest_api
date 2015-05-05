a429c4b055822430a76432729b909929d2a8a38a
Andrea Pierleoni <andreap@ebi.ac.uk>
2015-05-05 13:07:16 +0100

    updated dependency

--- update_requirements.py
+++ update_requirements.py
@@ -4,6 +4,6 @@ with open("requirements.txt", "w") as f:
     for dist in pip.get_installed_distributions():
         req = dist.as_requirement()
         if 'flask-restful-swagger' in str(req):
-            f.write("git+https://github.com/CTTV/flask-restful-swagger.git#egg=CTTV-swaggerui\n")
+            f.write("-e git+https://github.com/CTTV/flask-restful-swagger.git@657faa7377f5dcf7718f4e094d50aa2dd86999cf#egg=flask_restful_swagger-master\n")
         else:
             f.write(str(req) + "\n")
