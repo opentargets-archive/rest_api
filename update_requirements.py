e536d934262be6d0cf06a1369b5965d7c1cc98d6
Andrea Pierleoni <andreap@ebi.ac.uk>
2015-05-05 12:57:21 +0100

    using forked flask-restful-swagger

--- update_requirements.py
+++ update_requirements.py
@@ -3,4 +3,7 @@ import pip
 with open("requirements.txt", "w") as f:
     for dist in pip.get_installed_distributions():
         req = dist.as_requirement()
-        f.write(str(req) + "\n")
+        if 'flask-restful-swagger' in str(req):
+            f.write("git+https://github.com/CTTV/flask-restful-swagger.git#egg=CTTV-swaggerui\n")
+        else:
+            f.write(str(req) + "\n")
