16ded524ea7aba3608737eb4138d2865a2b979a2
Andrea Pierleoni <andreap@ebi.ac.uk>
2015-10-01 15:19:31 +0100

    return status code 203 if the status is not 'ok'. this will prevent caching for partial content

--- app/common/response_templates.py
+++ app/common/response_templates.py
@@ -23,23 +23,25 @@ class CTTVResponse():
         :return:
         '''
 
-
+        status = 200
+        if result.status != ['ok']:
+            status = 203
 
         if type == ResponseType.JSON:
             resp = Response(response=result.toJSON(),
-                            status=200,
+                            status=status,
                             mimetype="application/json")
         elif type == ResponseType.XML:
             resp = Response(response=result.toXML(),
-                            status=200,
+                            status=status,
                             mimetype="text/xml")
         elif type == ResponseType.CSV:
             resp = Response(response=result.toCSV(),
-                            status=200,
+                            status=status,
                             mimetype="text/csv")
         else:
             resp = Response(response=str(result),
-                            status=200,
+                            status=status,
                             mimetype="application/json")
         if took > 0.5:
             cache_time = str(int(3600*took))# set cache to last one our for each second spent in the request
