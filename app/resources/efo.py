f5abb3b8da83b7f295d03a53ec77dc3f62804cc3
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-02-23 15:31:16 +0000

    bugfix

--- app/resources/efo.py
+++ app/resources/efo.py
@@ -3,6 +3,7 @@ import json
 from app.common.auth import is_authenticated
 from app.common.rate_limit import rate_limit
 from app.common.response_templates import CTTVResponse
+from app.common.results import RawResult
 
 __author__ = 'andreap'
 from flask import current_app
@@ -26,7 +27,7 @@ class EfoLabelFromCode(restful.Resource):
         es = current_app.extensions['esquery']
         res = es.get_efo_info_from_code(disease_id)
         if res:
-            return CTTVResponse.OK(json.dumps(res[0]),
+            return CTTVResponse.OK(RawResult(json.dumps(res[0])),
                                    took=time.time() - start_time)
         else:
             abort(404, message="EFO code %s cannot be found"%disease_id)
