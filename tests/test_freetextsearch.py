4b7274b810c97605bc88e8e73edde34e8fb01175
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-04-15 17:53:55 +0100

    fix search bug when no reciprocal can be found

--- tests/test_freetextsearch.py
+++ tests/test_freetextsearch.py
@@ -79,7 +79,10 @@ class FreeTextSearchTestCase(GenericTestCase):
         self._assert_quicksearch_result(json_response,
                                    'BRAF',
                                    'B-Raf proto-oncogene, serine/threonine kinase',
-                                   'Protein kinase involved in the transduction of mitogenic signals from the cell membrane to the nucleus. May play a role in the postsynaptic responses of hippocampal neuron. Phosphorylates MAP2K1, and thereby contributes to the MAP kinase signal transduction pathway.',
+                                   'Protein kinase involved in the transduction of mitogenic signals from the cell'
+                                   ' membrane to the nucleus. May play a role in the postsynaptic responses of '
+                                   'hippocampal neuron. Phosphorylates MAP2K1, and thereby contributes to the MAP '
+                                   'kinase signal transduction pathway.',
                                    680)
 
 
@@ -94,10 +97,30 @@ class FreeTextSearchTestCase(GenericTestCase):
         self._assert_quicksearch_result(json_response,
                                    'asthma',
                                    'asthma',
-                                   "A bronchial disease that is characterized by chronic inflammation and narrowing of the airways, which is caused by a combination of environmental and genetic factors resulting in recurring periods of wheezing (a whistling sound while breathing), chest tightness, shortness of breath, mucus production and coughing. The symptoms appear due to a variety of triggers such as allergens, irritants, respiratory infections, weather changes, excercise, stress, reflux disease, medications, foods and emotional anxiety.",
+                                   "A bronchial disease that is characterized by chronic inflammation and narrowing of "
+                                   "the airways, which is caused by a combination of environmental and genetic factors "
+                                   "resulting in recurring periods of wheezing (a whistling sound while breathing), "
+                                   "chest tightness, shortness of breath, mucus production and coughing. The symptoms "
+                                   "appear due to a variety of triggers such as allergens, irritants, respiratory "
+                                   "infections, weather changes, excercise, stress, reflux disease, medications, "
+                                   "foods and emotional anxiety.",
                                    2086)
 
+    def testQuickSearchCancer(self):
 
+        response = self._make_request('/api/latest/private/quicksearch',
+                                      data={'q': 'cancer'},
+                                      token=self._AUTO_GET_TOKEN)
+
+        self.assertTrue(response.status_code == 200)
+        json_response = json.loads(response.data.decode('utf-8'))
+        self._assert_quicksearch_result(json_response,
+                                        'cancer',
+                                        'cancer',
+                                        "A malignant neoplasm in which new abnormal tissue grow by excessive cellular "
+                                        "division and proliferation more rapidly than normal and continues to grow "
+                                        "after the stimuli that initiated the new growth cease.",
+                                        20000)
 
     def testAutocomplete(self):
         response= self._make_request('/api/latest/private/autocomplete',
