6ef7a1488d0cd908aaf32f17c8e5df06d90e92a4
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-05-23 16:09:18 +0100

    Squashed commit of the following:
    commit 31ac882a038ed856cc00d6c07d8ee3bd70bc5a9f
    Author: Andrea Pierleoni <andreap@ebi.ac.uk>
    Date:   Mon May 23 15:30:51 2016 +0100
    
        do not overwrite real ip
    
    commit 452cf0c52ea7a488ca6f165db22b705f2aca0904
    Author: Andrea Pierleoni <andreap@ebi.ac.uk>
    Date:   Mon May 23 13:34:24 2016 +0100
    
        fixed real ip detection, and improved docs
    
    commit 80f19d319f6b60962e0da5bf5a0fedf8a4034558
    Author: Andrea Pierleoni <andreap@ebi.ac.uk>
    Date:   Tue May 17 10:41:18 2016 +0100
    
        forward user ip  and host in nginx

--- app/common/rate_limit.py
+++ app/common/rate_limit.py
@@ -74,9 +74,19 @@ class RateLimiter(object):
                 self.auth_token_payload = dict(app_name=request.form['app_name'],
                                                secret=request.form['secret'])
     def _get_remote_addr(self):
-        remote_addr =  request.environ.get('REMOTE_ADDR')
-        if remote_addr is None:
-            remote_addr = '127.0.0.1'
+        real_ip = request.headers.get("X-Real-IP")
+        if real_ip:
+            remote_addr = real_ip
+        elif not request.headers.getlist("X-Forwarded-For"):
+            if request.remote_addr is None:
+                remote_addr = '127.0.0.1'
+            else:
+                remote_addr = request.remote_addr
+        else:
+            #securely log all the forwarded ips.
+            #can be improved if a fixed number of proxied is known http://esd.io/blog/flask-apps-heroku-real-ip-spoofing.html
+            #TODO: change to a known number of proxy if env is production
+            remote_addr = ','.join(request.headers.getlist("X-Forwarded-For"))
         return str(remote_addr)
 
 
