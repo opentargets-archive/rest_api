2e2883f91bad3ea65cd0c037ed560afbd7cea46b
Andrea Pierleoni <andreap@ebi.ac.uk>
2016-08-09 10:01:44 +0100

    bugfix

--- app/common/scoring_conf.py
+++ app/common/scoring_conf.py
@@ -5,7 +5,6 @@ class DataSourceScoring():
 
     def __init__(self, app):
         self.methods = app.config['DATASOURCE_SCORING_METHOD']
-        self.weights = app.config['SCORING_WEIGHTS']
 
 class ScoringMethods():
 
