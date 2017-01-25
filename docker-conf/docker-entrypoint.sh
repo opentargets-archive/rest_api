3676b857e7369bc7a0b11bcae38804b196e5256f
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-01-25 12:34:44 +0000

    works with what is available if cannot increase max socker connection limit

--- docker-conf/docker-entrypoint.sh
+++ docker-conf/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-sysctl -w net.core.somaxconn=65535
+sysctl -w net.core.somaxconn=65535 || echo "Could not increase maximum connection, try in privileged mode"
+OUTPUT="$(sysctl net.core.somaxconn)"
+echo OUTPUT
+export SOCKET_MAX_CONN="${OUTPUT##* }"
 
-exec "$@"
+exec "$@"./
