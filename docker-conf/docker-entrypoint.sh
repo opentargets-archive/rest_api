511c015fb8eeaa05becc6eb474f7111a8e825e57
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-01-25 12:57:11 +0000

    echo value of connections

--- docker-conf/docker-entrypoint.sh
+++ docker-conf/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 sysctl -w net.core.somaxconn=65535 || echo "Could not increase maximum connection, try in privileged mode"
 OUTPUT="$(sysctl net.core.somaxconn)"
-echo OUTPUT
+echo $OUTPUT
 export SOCKET_MAX_CONN="${OUTPUT##* }"
 
 exec "$@"
