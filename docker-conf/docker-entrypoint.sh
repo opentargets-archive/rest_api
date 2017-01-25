fe97f0f06458eef6267204ecbcbb18aec6fffacb
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-01-25 23:04:53 +0000

    move source code in entryoint

--- docker-conf/docker-entrypoint.sh
+++ docker-conf/docker-entrypoint.sh
@@ -5,5 +5,6 @@ sysctl -w net.core.somaxconn=65535 || echo "Could not increase maximum connectio
 OUTPUT="$(sysctl net.core.somaxconn)"
 echo $OUTPUT
 export SOCKET_MAX_CONN="${OUTPUT##* }"
+cp -r /pipeline/source/* /var/www/app/ || echo "no source to copy"
 
 exec "$@"
