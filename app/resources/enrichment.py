a44954d5080a574a3d48dfb2d9844bb8351b6ddd
Andrea Pierleoni <andreap@ebi.ac.uk>
2017-02-14 15:35:11 +0000

    restrucutred code to use new endpoint

---
+++ app/resources/enrichment.py
@@ -0,0 +1,72 @@
+from flask.ext.restful.inputs import boolean
+from flask.ext.restful.reqparse import Argument
+from app.common import boilerplate
+
+
+from flask import current_app, request
+from flask.ext import restful
+from flask.ext.restful import abort, fields, marshal,marshal_with
+from flask.ext.restful import reqparse
+from app.common.auth import is_authenticated
+from app.common.rate_limit import rate_limit
+from app.common.request_templates import FilterTypes
+from app.common.response_templates import CTTVResponse
+from types import *
+import time
+
+
+__author__ = 'andreap'
+
+MAX_ELEMENT_SIZE = 100
+
+class EnrichmentTargets(restful.Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('target', type=str, action='append', required=True, )
+
+    @is_authenticated
+    @rate_limit
+    def get(self):
+        """
+        Get enriched disease from a set of targets
+        """
+
+        args = self.parser.parse_args()
+        targets = args['targets'][:MAX_ELEMENT_SIZE]
+        return self.get_enrichment_for_targets(targets)
+
+    @is_authenticated
+    @rate_limit
+    def post(self ):
+        """
+        Get enriched disease from a set of targets
+
+        """
+
+        args = request.get_json(force=True)
+        self.remove_empty_params(args)
+        targets = args['targets'][:MAX_ELEMENT_SIZE]
+
+
+        return self.get_enrichment_for_targets(targets)
+
+
+    def get_enrichment_for_targets(self, targets):
+        es = current_app.extensions['esquery']
+
+        res = es.get_enrichment_for_targets(targets)
+        if not res:
+            abort(404, message='Cannot find diseases for targets %s'%str(targets))
+        return CTTVResponse.OK(res)
+
+
+    def remove_empty_params(self,args):
+        for k,v in args.items():
+            if isinstance(v, list):
+                if len(v)>0:
+                    drop = True
+                    for i in v:
+                        if i != '':
+                            drop =False
+                    if drop:
+                        del args[k]
---
+++ tests/test_enrichment.py
@@ -0,0 +1,63 @@
+import json
+
+from tests import GenericTestCase
+
+
+class AssociationTestCase(GenericTestCase):
+
+    def testAssociationTargetsEnrichmentNone(self):
+        target = ["ENSG00000073756", "ENSG00000095303", "ENSG00000280151", "ENSG00000108219", "ENSG00000198369",
+                  "ENSG00000105401", "ENSG00000134882", "ENSG00000104998", "ENSG00000181915", "ENSG00000119919",
+                  "ENSG00000105397", "ENSG00000178623", "ENSG00000107968", "ENSG00000133195", "ENSG00000171522",
+                  "ENSG00000116288", "ENSG00000169508", "ENSG00000143365", "ENSG00000095110", "ENSG00000109787",
+                  "ENSG00000179344", "ENSG00000153064", "ENSG00000108094", "ENSG00000116833", "ENSG00000151151",
+                  "ENSG00000111537", "ENSG00000114859", "ENSG00000012779", "ENSG00000169220", "ENSG00000103375",
+                  "ENSG00000189067", "ENSG00000108423", "ENSG00000133703", "ENSG00000138031", "ENSG00000178828",
+                  "ENSG00000197114", "ENSG00000130592", "ENSG00000100368", "ENSG00000164308", "ENSG00000111424",
+                  "ENSG00000108688", "ENSG00000181634", "ENSG00000164512", "ENSG00000182256", "ENSG00000113327",
+                  "ENSG00000163285", "ENSG00000079263", "ENSG00000115232", "ENSG00000100311", "ENSG00000143622"]
+        response = self._make_request('/api/latest/private/enrichment/targets',
+                                      data={'target': target,
+                                            'method': 'POST',
+                                            'no_cache': True
+                                            },
+                                      token=self._AUTO_GET_TOKEN)
+        self.assertTrue(response.status_code == 200)
+        json_response = json.loads(response.data.decode('utf-8'))
+        self.assertIn('disease_enrichment', json_response)
+        self.assertTrue(len(json_response['disease_enrichment']) == 0)
+
+
+    def testAssociationTargetEnrichmentGet(self):
+        target = ["ENSG00000073756", "ENSG00000095303", "ENSG00000280151", "ENSG00000108219", "ENSG00000198369",
+                  "ENSG00000105401", "ENSG00000134882", "ENSG00000104998", "ENSG00000181915", "ENSG00000119919",
+                  "ENSG00000105397", "ENSG00000178623", "ENSG00000107968", "ENSG00000133195", "ENSG00000171522",
+                  "ENSG00000116288", "ENSG00000169508", "ENSG00000143365", "ENSG00000095110", "ENSG00000109787",
+                  "ENSG00000179344", "ENSG00000153064", "ENSG00000108094", "ENSG00000116833", "ENSG00000151151",
+                  "ENSG00000111537", "ENSG00000114859", "ENSG00000012779", "ENSG00000169220", "ENSG00000103375",
+                  "ENSG00000189067", "ENSG00000108423", "ENSG00000133703", "ENSG00000138031", "ENSG00000178828",
+                  "ENSG00000197114", "ENSG00000130592", "ENSG00000100368", "ENSG00000164308", "ENSG00000111424",
+                  "ENSG00000108688", "ENSG00000181634", "ENSG00000164512", "ENSG00000182256", "ENSG00000113327",
+                  "ENSG00000163285", "ENSG00000079263", "ENSG00000115232", "ENSG00000100311", "ENSG00000143622"]
+        response = self._make_request('/api/latest/private/enrichment/targets',
+                                      data={'target': target,
+                                            'targets_enrichment': "simple",
+                                            'no_cache': True
+                                            },
+                                      token=self._AUTO_GET_TOKEN)
+
+        self.assertTrue(response.status_code == 200)
+        json_response = json.loads(response.data.decode('utf-8'))
+
+        self.assertIn('disease_enrichment', json_response)
+
+        self.assertIsNotNone(json_response['disease_enrichment'])
+
+        min_score = 0
+        for disease in json_response['disease_enrichment']:
+            self.assertIsNotNone(disease['score'])
+            score = disease['score']
+            self.assertGreaterEqual(score, min_score)
+            min_score = score
+            self.assertIsNotNone(disease['counts'])
+            self.assertIsNotNone(disease['label'])
