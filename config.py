13466e774de072d1b349bdaea4b0e23021e5a22a
Priyanka Wankar <priyanka.wankar@biogen.com>
2017-07-10 16:54:40 -0400

    Enable nferx datatype

--- config.py
+++ config.py
@@ -87,7 +87,8 @@ class Config:
     DATATYPES['somatic_mutation'] = ['cancer_gene_census','eva_somatic','intogen']
     DATATYPES['known_drug'] = ['chembl',]
     DATATYPES['literature'] = ['europepmc']
-    DATATYPE_ORDERED=['genetic_association','somatic_mutation','known_drug','rna_expression','affected_pathway','animal_model', 'literature']
+    DATATYPES['literature_nferx'] = ['nferx']
+    DATATYPE_ORDERED=['genetic_association','somatic_mutation','known_drug','rna_expression','affected_pathway','animal_model', 'literature', 'literature_nferx']
     # DATATYPES['protein_expression'] = ['hpa']
 
     DATASOURCE_SCORING_METHOD = defaultdict(lambda: ScoringMethods.SUM)
