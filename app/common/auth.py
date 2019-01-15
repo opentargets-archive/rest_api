bfe2b8d8691a6e39323a1e934723407b16209fac
Miguel C <mkarmona@gmail.com>
2019-01-15 14:25:49 +0000

    refactor: clean up

--- app/common/auth.py
+++ app/common/auth.py
@@ -1,16 +1,10 @@
-from datetime import datetime
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadData
-from itsdangerous import SignatureExpired, BadSignature
 from flask import current_app, request
-from flask_restful import abort, wraps
 import base64
 import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
 import json
 
-from app.common.signals import LogApiTokenInvalidDomain, LogApiTokenExpired, LogApiTokenInvalid
-from app.common.exceptions import TokenExpired
 from config import Config
 __author__ = 'andreap'
 
@@ -21,8 +15,6 @@ class AuthKey(object):
                  app_name='',
                  secret='',
                  domain='',
-                 short_window_rate=0,
-                 long_window_rate=0,
                  users_allowed="False",
                  reference='',
                  **kwargs):
@@ -67,100 +59,4 @@ class AESCipher:
 
 def get_domain():
     return request.environ.get('HTTP_HOST').split(':')[0]
-
-class TokenAuthentication():
-
-    app_name='cttv-rest-api'
-    EXPIRED = 'expired'
-
-    @staticmethod
-    def _autenticate(auth_data):
-        if auth_data['secret'] and auth_data['app_name']:
-            auth_key = AuthKey(**auth_data)
-            if current_app.extensions['redis-user'].exists(auth_key.get_key()):
-                auth_key.get_loaded_data()
-                domain = get_domain()
-                if auth_key.domain:
-                    for allowed_domain in auth_key.domain.split('|'):
-                        if allowed_domain[0]=='*':
-                            if domain.endswith(allowed_domain[1:]):
-                                return True
-                        else:
-                            if domain==allowed_domain:
-                                return True
-                else:
-                    return True
-        return False
-
-
-    @staticmethod
-    def _prepare_payload(api_name, auth_data):
-        payload = {'api_name': api_name,
-                   'app_name': auth_data['app_name'],
-                   'secret': auth_data['secret'],
-                   'domain': get_domain()}
-        if 'uid' in auth_data:
-            payload['uid'] = auth_data['uid']
-        return payload
-
-    @staticmethod
-    def get_payload_from_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        cipher = AESCipher(current_app.config['SECRET_KEY'][:16])
-        try:
-            data = json.loads(cipher.decrypt(s.loads(token)))
-            return data
-        except SignatureExpired, se:
-            time_offset = (datetime.now()- se.date_signed).total_seconds()
-            current_app.logger.error('token expired: %s. signature date %s. offset with current date = %s'%(se.message,str(se.date_signed),str(time_offset)))
-            current_app.logger.error('current date %s, token date %s'%(str(datetime.now()), str(se.date_signed)))
-            if -1<= time_offset < 0:#allow for 1 seconds out of sync machines
-                current_app.logger.info('token time offset within grace period. allowing auth')
-                return json.loads(cipher.decrypt(se.payload))
-            else:
-                LogApiTokenExpired()
-                # raise SignatureExpired(se)
-                raise TokenExpired()
-                # abort(419, message = 'Authentication expired.')
-        except BadSignature, e:
-            current_app.logger.error('bad signature in token')
-            encoded_payload = e.payload
-            if encoded_payload is not None:
-                try:
-                    decoded_payload = s.load_payload(encoded_payload)
-                    payload= json.loads(cipher.decrypt(decoded_payload))
-                    LogApiTokenInvalid(payload)
-                except BadData:
-                    LogApiTokenInvalid(dict(error='bad data in token',
-                                            token=token))
-            abort(401, message = 'bad signature in token')
-
-
-
-
-    @classmethod
-    def get_auth_token(cls, api_name='', expiry=600, salt='', auth_data ={}):
-        """
-
-        :rtype : str token
-        """
-        if cls._autenticate(auth_data):
-            payload = cls._prepare_payload(api_name,auth_data)
-            s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiry, )
-            cipher = AESCipher(current_app.config['SECRET_KEY'][:16])
-            token = s.dumps(cipher.encrypt(json.dumps(payload)))
-            current_app.logger.info('token served', extra=dict(token=token))
-            return json.dumps(dict(token=token))
-        abort(401, message='authentication credentials not valid')
-
-    @classmethod
-    def is_valid(cls,token):
-        payload = cls.get_payload_from_token(token)
-        if payload:
-            if payload['domain'] != get_domain():
-                current_app.logger.error("bad domain in token: got %s expecting %s"%(payload['domain'],get_domain()))
-                LogApiTokenInvalidDomain(payload)
-                abort(401)
-            return True
-        abort(401)
 
